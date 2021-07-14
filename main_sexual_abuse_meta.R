10bb882f733673e17aab1761b231d4b85744771f
mzarchev <mzarchev@gmail.com>
2021-07-14 10:41:53 +0200

    Simplified

--- main_sexual_abuse_meta.R
+++ main_sexual_abuse_meta.R
@@ -8,40 +8,39 @@ library(brms)
 # Function with appropriate priors to fit to models
 brmsModel <- function(df){
   brm(data = df, family = binomial,
-      n|trials(N) ~ (1|Author), 
+      n|trials(N) ~ (1|study), 
       prior = c(prior(normal(0, 2), class = Intercept),
                 prior(cauchy(0, 1), class = sd)),
-      iter = 10000, warmup = 2000, cores = 4, chains = 4,
+      iter = 1000, warmup = 200, cores = 4, chains = 2,
       control = list(adapt_delta = .99),
       seed = 14)
 }
 
-
 # Fitting the models ------------------------------------------------------
 
-# Fit for all sample types
 completeDF <-
-  read.csv("allData.csv") %>%  
+  # Read in data
+  read.csv("allData.csv") %>%
+  # Prep variables for model fitting
   select(-c(Lifetime, Adulthood)) %>% 
   mutate(study = paste0(str_to_lower(Population), "_", Author)) %>% 
   pivot_longer(c(Past.year, Adult), names_to = "period", values_to = "prev") %>%
-  group_by(Group, period) %>%
   mutate(n = round((prev/100)*N)) %>%
-  drop_na(prev) %>% 
+  drop_na(prev) %>%
+  # Group by sample type
+  group_by(Population, period) %>%
   nest() %>% 
-  mutate(title = paste0("Sexual abuse prevalence in ", Group, " samples during ", period)) %>% 
-  select(Group, period, title, data) %>%
-  mutate(mod = data %>% map(brmsModel))
-
-# Filter only populations with enough studies (>3 studies)
-models_DF <- 
-  completeDF %>%
-  ungroup() %>% 
-  filter(Group == "Mixed" | Group == "SA" | Group == "IPVmix" & period == "Adult") %>% 
-  mutate(plot = map(fit, forest_plot)) %>% 
-  select(-plotForest)
+  # Include populations with enough studies (>3 studies)
+  filter(Population == "Mixed" | Population == "SA" | 
+         Population == "IPVmix" & period == "Adult" |
+         Population == "Psychosis" & period == "Adult"
+         ) %>% 
+  mutate(title = paste0("Sexual abuse prevalence in ", Population, " samples during ", period)) %>% 
+  select(Population, period, title, data) %>%
+  # Fit model for each sample type
+  mutate(mod = map(data, brmsModel))
 
 # Save
-saveRDS(models_DF, "models_DF.RData")
+saveRDS(completeDF, "models_DF.RData")
 
 
